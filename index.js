8bee11622129adf6dc99d1e3964ade4668de26dd
Thiago Oliveira <thiago@dynamox.net>
2017-08-10 13:54:39 -0300

    Fixed ns

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-  'mixins': {
+  mixins: {
     'pagination-meta': require('./lib/mixins/pagination-meta'),
     'soft-delete': require('./lib/mixins/soft-delete'),
     'timestamp': require('./lib/mixins/timestamp')
   },
-  'test': {
+  testing: {
     'auth': require('./lib/testing/auth')
   }
 };
